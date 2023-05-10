ad39357d41e752a40f7e1e5f1e4481b7e626613c
uemue <7414847+uemue@users.noreply.github.com>
2023-05-10 23:08:21 +0900

    Add tests

---
+++ src/convertNodeToScrapboxText.test.ts
@@ -0,0 +1,140 @@
+import { convertNodeToScrapboxText } from './convertNodeToScrapboxText';
+
+describe('convertNodeToScrapboxText', () => {
+  test('converts a simple text node', () => {
+    const node = document.createTextNode('hello');
+    expect(convertNodeToScrapboxText(node, '')).toBe('hello');
+  });
+
+  test('converts a <h1> node', () => {
+    const node = document.createElement('h1');
+    node.appendChild(document.createTextNode('hello'));
+    expect(convertNodeToScrapboxText(node, '')).toBe('[****** hello]\n');
+  });
+
+  test('converts a <h2> node', () => {
+    const node = document.createElement('h2');
+    node.appendChild(document.createTextNode('hello'));
+    expect(convertNodeToScrapboxText(node, '')).toBe('[***** hello]\n');
+  });
+
+  test('converts a <h3> node', () => {
+    const node = document.createElement('h3');
+    node.appendChild(document.createTextNode('hello'));
+    expect(convertNodeToScrapboxText(node, '')).toBe('[**** hello]\n');
+  });
+
+  test('converts a <h4> node', () => {
+    const node = document.createElement('h4');
+    node.appendChild(document.createTextNode('hello'));
+    expect(convertNodeToScrapboxText(node, '')).toBe('[*** hello]\n');
+  });
+
+  test('converts a <h5> node', () => {
+    const node = document.createElement('h5');
+    node.appendChild(document.createTextNode('hello'));
+    expect(convertNodeToScrapboxText(node, '')).toBe('[** hello]\n');
+  });
+
+  test('converts a <h6> node', () => {
+    const node = document.createElement('h6');
+    node.appendChild(document.createTextNode('hello'));
+    expect(convertNodeToScrapboxText(node, '')).toBe('[* hello]\n');
+  });
+
+  test('converts a <p> node', () => {
+    const node = document.createElement('p');
+    node.appendChild(document.createTextNode('hello'));
+    expect(convertNodeToScrapboxText(node, '')).toBe('hello\n');
+  });
+
+  test('converts a <img> node', () => {
+    const node = document.createElement('img');
+    node.setAttribute('src', 'https://example.com/image.png');
+    expect(convertNodeToScrapboxText(node, '')).toBe(
+      '[https://example.com/image.png]\n'
+    );
+  });
+
+  test('converts <ul> and <li> nodes', () => {
+    const node = document.createElement('div');
+    node.innerHTML = `
+      <ul>
+        <li>item1</li>
+        <li>item2</li>
+      </ul>
+    `;
+
+    // expected:
+    //  item1
+    //  item2
+
+    expect(convertNodeToScrapboxText(node, '')).toBe(' item1\n item2\n');
+  });
+
+  test('converts <ul> and <li> nodes with nested <ul> and <li> nodes', () => {
+    const node = document.createElement('div');
+    node.innerHTML = `
+      <ul>
+        <li>item1</li>
+        <li>item2
+          <ul>
+            <li>nestedItem1</li>
+            <li>nestedItem2</li>
+          </ul>
+        </li>
+      </ul>
+    `;
+
+    // expected:
+    //  item1
+    //  item2
+    //   nestedItem1
+    //   nestedItem2
+
+    expect(convertNodeToScrapboxText(node, '')).toBe(
+      ' item1\n item2\n  nestedItem1\n  nestedItem2\n'
+    );
+  });
+
+  test('converts <ol> and <li> nodes', () => {
+    const node = document.createElement('div');
+    node.innerHTML = `
+      <ol>
+        <li>item1</li>
+        <li>item2</li>
+      </ol>
+    `;
+
+    // expected:
+    //  1. item1
+    //  2. item2
+
+    expect(convertNodeToScrapboxText(node, '')).toBe(' 1. item1\n 2. item2\n');
+  });
+
+  test('converts <ol> and <li> nodes with nested <ol> and <li> nodes', () => {
+    const node = document.createElement('div');
+    node.innerHTML = `
+      <ol>
+        <li>item1</li>
+        <li>item2
+          <ol>
+            <li>nestedItem1</li>
+            <li>nestedItem2</li>
+          </ol>
+        </li>
+      </ol>
+    `;
+
+    // expected:
+    //  1. item1
+    //  2. item2
+    //     1. nestedItem1
+    //     2. nestedItem2
+
+    expect(convertNodeToScrapboxText(node, '')).toBe(
+      ' 1. item1\n 2. item2\n  1. nestedItem1\n  2. nestedItem2\n'
+    );
+  });
+});

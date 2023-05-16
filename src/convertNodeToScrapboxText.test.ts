02e64280e30bc9fcafaadc3e498c2c2e34add275
uemue <7414847+uemue@users.noreply.github.com>
2023-05-17 08:27:39 +0900

    Refactor convertNodeToScrapboxText function to remove indent parameter

--- src/convertNodeToScrapboxText.test.ts
+++ src/convertNodeToScrapboxText.test.ts
@@ -3,55 +3,55 @@ import { convertNodeToScrapboxText } from './convertNodeToScrapboxText';
 describe('convertNodeToScrapboxText', () => {
   test('converts a simple text node', () => {
     const node = document.createTextNode('hello');
-    expect(convertNodeToScrapboxText(node, '')).toBe('hello');
+    expect(convertNodeToScrapboxText(node)).toBe('hello');
   });
 
   test('converts a <h1> node', () => {
     const node = document.createElement('h1');
     node.appendChild(document.createTextNode('hello'));
-    expect(convertNodeToScrapboxText(node, '')).toBe('[****** hello]\n');
+    expect(convertNodeToScrapboxText(node)).toBe('[****** hello]\n');
   });
 
   test('converts a <h2> node', () => {
     const node = document.createElement('h2');
     node.appendChild(document.createTextNode('hello'));
-    expect(convertNodeToScrapboxText(node, '')).toBe('[***** hello]\n');
+    expect(convertNodeToScrapboxText(node)).toBe('[***** hello]\n');
   });
 
   test('converts a <h3> node', () => {
     const node = document.createElement('h3');
     node.appendChild(document.createTextNode('hello'));
-    expect(convertNodeToScrapboxText(node, '')).toBe('[**** hello]\n');
+    expect(convertNodeToScrapboxText(node)).toBe('[**** hello]\n');
   });
 
   test('converts a <h4> node', () => {
     const node = document.createElement('h4');
     node.appendChild(document.createTextNode('hello'));
-    expect(convertNodeToScrapboxText(node, '')).toBe('[*** hello]\n');
+    expect(convertNodeToScrapboxText(node)).toBe('[*** hello]\n');
   });
 
   test('converts a <h5> node', () => {
     const node = document.createElement('h5');
     node.appendChild(document.createTextNode('hello'));
-    expect(convertNodeToScrapboxText(node, '')).toBe('[** hello]\n');
+    expect(convertNodeToScrapboxText(node)).toBe('[** hello]\n');
   });
 
   test('converts a <h6> node', () => {
     const node = document.createElement('h6');
     node.appendChild(document.createTextNode('hello'));
-    expect(convertNodeToScrapboxText(node, '')).toBe('[* hello]\n');
+    expect(convertNodeToScrapboxText(node)).toBe('[* hello]\n');
   });
 
   test('converts a <p> node', () => {
     const node = document.createElement('p');
     node.appendChild(document.createTextNode('hello'));
-    expect(convertNodeToScrapboxText(node, '')).toBe('hello\n');
+    expect(convertNodeToScrapboxText(node)).toBe('hello\n');
   });
 
   test('converts a <img> node', () => {
     const node = document.createElement('img');
     node.setAttribute('src', 'https://example.com/image.png');
-    expect(convertNodeToScrapboxText(node, '')).toBe(
+    expect(convertNodeToScrapboxText(node)).toBe(
       '[https://example.com/image.png]\n'
     );
   });
@@ -69,7 +69,7 @@ describe('convertNodeToScrapboxText', () => {
     //  item1
     //  item2
 
-    expect(convertNodeToScrapboxText(node, '')).toBe(' item1\n item2\n');
+    expect(convertNodeToScrapboxText(node)).toBe(' item1\n item2\n');
   });
 
   test('converts <ul> and <li> nodes with nested <ul> and <li> nodes', () => {
@@ -92,7 +92,7 @@ describe('convertNodeToScrapboxText', () => {
     //   nestedItem1
     //   nestedItem2
 
-    expect(convertNodeToScrapboxText(node, '')).toBe(
+    expect(convertNodeToScrapboxText(node)).toBe(
       ' item1\n item2\n  nestedItem1\n  nestedItem2\n'
     );
   });
@@ -110,7 +110,7 @@ describe('convertNodeToScrapboxText', () => {
     //  1. item1
     //  2. item2
 
-    expect(convertNodeToScrapboxText(node, '')).toBe(' 1. item1\n 2. item2\n');
+    expect(convertNodeToScrapboxText(node)).toBe(' 1. item1\n 2. item2\n');
   });
 
   test('converts <ol> and <li> nodes with nested <ol> and <li> nodes', () => {
@@ -133,7 +133,7 @@ describe('convertNodeToScrapboxText', () => {
     //     1. nestedItem1
     //     2. nestedItem2
 
-    expect(convertNodeToScrapboxText(node, '')).toBe(
+    expect(convertNodeToScrapboxText(node)).toBe(
       ' 1. item1\n 2. item2\n  1. nestedItem1\n  2. nestedItem2\n'
     );
   });
@@ -145,21 +145,21 @@ describe('convertNodeToScrapboxText', () => {
         <p>hello</p>
       </blockquote>
     `;
-    expect(convertNodeToScrapboxText(node, '')).toBe('>hello\n');
+    expect(convertNodeToScrapboxText(node)).toBe('> hello\n');
   });
 
   test('converts <a> nodes', () => {
     const node = document.createElement('a');
     node.setAttribute('href', 'https://example.com');
     node.appendChild(document.createTextNode('example'));
-    expect(convertNodeToScrapboxText(node, '')).toBe(
+    expect(convertNodeToScrapboxText(node)).toBe(
       '[example https://example.com]'
     );
   });
-});
 
-test('converts <strong> nodes', () => {
-  const node = document.createElement('strong');
-  node.appendChild(document.createTextNode('hello'));
-  expect(convertNodeToScrapboxText(node, '')).toBe('[* hello]');
+  test('converts <strong> nodes', () => {
+    const node = document.createElement('strong');
+    node.appendChild(document.createTextNode('hello'));
+    expect(convertNodeToScrapboxText(node)).toBe('[* hello]');
+  });
 });
--- src/convertNodeToScrapboxText.ts
+++ src/convertNodeToScrapboxText.ts
@@ -1,5 +1,5 @@
 // convert a element to a Scrapbox text. This function walks through the element and its children recursively.
-export function convertNodeToScrapboxText(node: Node, indent: string): string {
+export function convertNodeToScrapboxText(node: Node): string {
   if (node.nodeType === Node.TEXT_NODE) {
     return node.textContent?.trim() || '';
   } else if (node.nodeType !== Node.ELEMENT_NODE) {
@@ -11,74 +11,77 @@ export function convertNodeToScrapboxText(node: Node, indent: string): string {
 
   switch (element.tagName) {
     case 'H1':
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return indent + `[****** ${childContent}]\n`;
-    case 'H2':
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return indent + `[***** ${childContent}]\n`;
-    case 'H3':
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return indent + `[**** ${childContent}]\n`;
-    case 'H4':
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return indent + `[*** ${childContent}]\n`;
-    case 'H5':
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return indent + `[** ${childContent}]\n`;
-    case 'H6':
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return indent + `[* ${childContent}]\n`;
-    case 'P':
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return indent + `${childContent}\n`;
-    case 'IMG':
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `[****** ${childContent}]\n`;
+      case 'H2':
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `[***** ${childContent}]\n`;
+      case 'H3':
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `[**** ${childContent}]\n`;
+      case 'H4':
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `[*** ${childContent}]\n`;
+      case 'H5':
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `[** ${childContent}]\n`;
+      case 'H6':
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `[* ${childContent}]\n`;
+      case 'P':
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `${childContent}\n`;
+      case 'IMG':
       return `[${element.getAttribute('src')}]\n`;
     case 'UL':
       for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent + ' ');
+        childContent += convertNodeToScrapboxText(child);
       }
-      return `${childContent}`;
+      const ulLines = childContent.trimEnd().split('\n');
+      const ulIndentedLines = ulLines.map((line) => ' ' + line);
+      return `${ulIndentedLines.join('\n')}\n`;
     case 'LI':
       for (const child of Array.from(element.childNodes)) {
         if (child.nodeName === 'UL' || child.nodeName === 'OL') {
           childContent += '\n';
         }
-        childContent += convertNodeToScrapboxText(child, indent);
+        childContent += convertNodeToScrapboxText(child);
       }
-      return indent + `${childContent.trimEnd()}\n`;
+      return `${childContent.trimEnd()}\n`;
     case 'OL':
       let counter = 1;
       for (const child of Array.from(element.childNodes)) {
         if (child.nodeName === 'LI') {
-          const childIndent = indent + ' ';
-          const item = convertNodeToScrapboxText(child, childIndent);
-          childContent += item.replace(
-            childIndent,
-            `${childIndent}${counter}. `
-          );
+          const item = convertNodeToScrapboxText(child);
+          const olLines = item.trimEnd().split('\n');
+          const [head, ...tail] = olLines;
+          childContent += ` ${counter}. ${head}\n`;
+          childContent += tail.map((line) => ' ' + line).join('\n');
           counter++;
         }
       }
-      return `${childContent}`;
+      return `${childContent.trimEnd()}\n`;
     case 'BLOCKQUOTE':
       for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent + '>');
+        childContent += convertNodeToScrapboxText(child);
       }
-      return `${childContent}\n`;
+      let lines = childContent.trimEnd().split('\n');
+      const indentedLines = lines.map((line) => '> ' + line);
+      return `${indentedLines.join('\n')}\n`;
     case 'CODE':
       //TODO: implement
       // インラインとブロックの両方があるな
@@ -91,28 +94,28 @@ export function convertNodeToScrapboxText(node: Node, indent: string): string {
     case 'TABLE':
       //TODO: implement
       return '';
-    case 'A':
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return `[${childContent} ${element.getAttribute('href')}]`;
-    case 'STRONG':
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return `[* ${childContent}]`;
-    case 'EM':
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return `[/ ${childContent}]`;
-    case 'CODE':
-      //TODO: implement
-      return '';
-    default:
-      for (const child of Array.from(element.childNodes)) {
-        childContent += convertNodeToScrapboxText(child, indent);
-      }
-      return `${childContent}`;
-  }
+      case 'A':
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `[${childContent} ${element.getAttribute('href')}]`;
+      case 'STRONG':
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `[* ${childContent}]`;
+      case 'EM':
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `[/ ${childContent}]`;
+      case 'CODE':
+        //TODO: implement
+        return '';
+      default:
+        for (const child of Array.from(element.childNodes)) {
+          childContent += convertNodeToScrapboxText(child);
+        }
+        return `${childContent}`;
+    }
 }

f330d18406c7b61e1f3e8ed4a7c25fafa9e8b76d
uemue <7414847+uemue@users.noreply.github.com>
2023-05-19 12:12:41 +0900

    Fix user & chatGPT icon

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
-import { convertNodeToScrapboxText } from './convertNodeToScrapboxText';
+import { convertNodeToScrapboxText } from "./convertNodeToScrapboxText";
 
 (async function () {
   // replace your_user_name to your Scrapbox user name
-  const userName = 'your_user_name';
-  const chatGPTName = 'ChatGPT';
+  const userName = "your_user_name";
+  const chatGPTName = "ChatGPT";
 
   function getMessageElements(page: Element): Element[] {
-    const messageElements = page.querySelectorAll('div.text-base');
+    const messageElements = page.querySelectorAll("div.text-base");
     return Array.from(messageElements);
   }
 
   function convertToScrapbox(messageElements: Element[]): string {
     const scrapboxTexts = messageElements.map((messageElement) => {
-      let scrapboxText = '';
+      let scrapboxText = "";
 
       if (isUserMessage(messageElement)) {
-        scrapboxText += `[{userName}.icon]`;
+        scrapboxText += `[${userName}.icon]`;
       } else {
-        scrapboxText += `[{chatGPTName}.icon]`;
+        scrapboxText += `[${chatGPTName}.icon]`;
       }
 
       const messageContentElement = messageElement.querySelector(
-        '.text-base > div:nth-child(2)'
+        ".text-base > div:nth-child(2)"
       );
 
       if (!messageContentElement) {
-        return '';
+        return "";
       }
 
-      scrapboxText += convertNodeToScrapboxText(messageContentElement, ' ');
+      scrapboxText += convertNodeToScrapboxText(messageContentElement, " ");
       return scrapboxText;
     });
 
-    return scrapboxTexts.join('\n');
+    return scrapboxTexts.join("\n");
   }
 
   function isUserMessage(messageElement: Element): boolean {
@@ -47,7 +47,7 @@ import { convertNodeToScrapboxText } from './convertNodeToScrapboxText';
     try {
       await navigator.clipboard.writeText(text);
     } catch (err) {
-      console.error('Failed to copy: ', err);
+      console.error("Failed to copy: ", err);
     }
   }
 
@@ -55,5 +55,5 @@ import { convertNodeToScrapboxText } from './convertNodeToScrapboxText';
   const scrapboxText = convertToScrapbox(messageElements);
   await copyToClipboard(scrapboxText);
 
-  alert('Copied to clipboard!');
+  alert("Copied to clipboard!");
 })();
